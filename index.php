37d776852e27d173168d35468349cdecabda24be
DutchGamingRoy <35993478+DutchGamingRoy@users.noreply.github.com>
2018-02-08 08:53:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <LINK href="stylesheet.css" rel="stylesheet" type="text/css">
+        <title>Roy's page</title>
+    </head>
+    <body>
+    <li>
+        <ul><a href="http://ao-groep10.000webhostapp.com">Home</a></ul>
+        <ul><a href="https://roys-website.000webhostapp.com/index.php">roy</a></ul>
+        <ul><a href="http://ao-groep10.000webhostapp.com/luuk.php">luuk</a></ul>
+        <ul><a href="http://marijnwelmerink.000webhostapp.com">marijn</a></ul>
+        <ul><a href="https://kjell.000webhostapp.com/kjell.php">kjell</a></ul>
+        <ul><a href="http://milenko.000webhostapp.com">milenko</a></ul>
+        <ul><a href="http://husta02.000webhostapp.com">hasan</a></ul>
+        <ul><a href="http://groep10.000webhostapp.com">damion</a></ul>
+    </li>
+        <h1>Roy Weghorst</h1>
+            <img src="roy.png" width="500px" height="500px">
+        <a href="thisisme.php">this is me</a>
+        <a href="quotes.php">quotes</a>
+        <a href="moreoperators.php"> more operators</a>
+        <a href="compare.php">compare</a>
+        <a href="scope.php">scope</a>
+        <a href="moreoperators.php">more operators</a>
+        <a href="operators.php">operators</a>
+        <a href="kaartspel.php">kaarten</a>
+    </body>
+</html>
---
+++ thisisme.php
@@ -0,0 +1,28 @@
+<HTML>
+<HEAD>
+
+    <TITLE>This is me</TITLE>
+
+</HEAD>
+
+<BODY>
+
+<img src="roy.png" alt="Foto" width="100">
+
+<?php
+
+$voornaam = 'Roy';
+$achternaam = 'Weghorst';
+$adres = '[redacted]';
+$huisnummer = '41';
+$plaats = 'Hengelo';
+$leeftijd = '16';
+
+echo "<h1>Naam: $voornaam $achternaam</h1>";
+echo "<h2>Adres: $adres $huisnummer $plaats</h2>";
+echo "<h3>Leeftijd: $leeftijd</h3>";
+
+?>
+
+</BODY>
+</HTML>
