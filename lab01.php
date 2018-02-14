2f0e4be003d515df3d8b4cbd3e28d6abd93cd9b3
DutchGamingRoy <35993478+DutchGamingRoy@users.noreply.github.com>
2018-02-14 11:16:36 +0100

    Add files via upload

---
+++ lab01.php
@@ -0,0 +1,10 @@
+<?php
+$breedte = 10;
+$lengte = 11;
+$hoogte = 5;
+$oppervlakte = $lengte * $breedte;
+$volume = $lengte * $breedte * $hoogte;
+
+echo "Containeroppervlakte is: " . $oppervlakte."<br>";
+echo "containervolume is: " . $volume;
+?>
